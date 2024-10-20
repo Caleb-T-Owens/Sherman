b0430d29fd70a1b50edc808a8ba0fcbdb64d1669
Caleb Owens <hello@calebowens.com>
2024-10-20 22:23:57 +0100

    Added slightly mad project cloner

---
+++ projects/project-cloner/README.md
@@ -0,0 +1,19 @@
+# project-cloner
+
+This is a distinctly dangerous "could break your computer" script that looks at
+`$HOME/sherman/projeccts/cloned.json` and attempts to clone the entries listed
+there. Any entries will get added to the `.gitignore` in the safe folder to
+
+## How to use this contraption:
+
+To install dependencies:
+
+```bash
+bun install
+```
+
+To run:
+
+```bash
+bun run index.ts
+```
---
+++ projects/project-cloner/index.ts
@@ -0,0 +1,58 @@
+import { $, env } from "bun";
+import { z } from "zod";
+
+const shermanDir = `${env.HOME}/sherman`;
+const projectsDir = `${shermanDir}/projects`;
+
+const cloneFile = `${projectsDir}/cloned.json`;
+const clonedRaw = await $`cat ${cloneFile}`.text();
+const clonedUnparsed = JSON.parse(clonedRaw);
+
+const clonedSchema = z.record(
+  z.string(),
+  z.object({
+    url: z.string(),
+  })
+);
+
+const cloned = clonedSchema.parse(clonedUnparsed);
+
+const gitignorePath = `${projectsDir}/.gitignore`;
+
+let gitignore: string[] = [];
+
+const { exitCode: gitignoreExists } =
+  await $`[ -e ${gitignorePath} ]`.nothrow();
+if (gitignoreExists === 0) {
+  gitignore = await Array.fromAsync(await $`cat ${gitignorePath}`.lines());
+}
+
+for (const folderName of Object.keys(cloned)) {
+  const gitignoreEntry = `${folderName}/`;
+  if (!gitignore.includes(gitignoreEntry)) {
+    gitignore.push(gitignoreEntry);
+  }
+}
+
+const joinedGitignore = gitignore.join("\n");
+await $`echo "${joinedGitignore}" > ${gitignorePath}`;
+
+for (const [folderName, object] of Object.entries(cloned)) {
+  const folderPath = `${projectsDir}/${folderName}`;
+
+  const { exitCode: projectExists } = await $`[ -e ${folderPath} ]`.nothrow();
+
+  if (projectExists === 0) {
+    console.log(`Project ${folderName} is already cloned`);
+    continue;
+  }
+
+  console.log(`Cloning ${folderName}...`);
+
+  const { exitCode: gitCloneStatus } =
+    await $`git clone ${object.url} ${folderPath} --recurse-submodules`.nothrow();
+
+  if (gitCloneStatus !== 0) {
+    console.log("Failed to clone ${folderName}");
+  }
+}
