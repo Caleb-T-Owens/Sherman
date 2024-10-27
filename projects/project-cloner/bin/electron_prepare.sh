12bd87c594347abb1c4e1ff8fa40e13420ac8d14
Caleb Owens <hello@calebowens.com>
2024-10-27 22:32:47 +0000

    Updated prepare script to not re-install bun

--- projects/project-cloner/bin/electron_prepare.sh
+++ projects/project-cloner/bin/electron_prepare.sh
@@ -1,2 +1,8 @@
-# Install/update bun
-curl -fsSL https://bun.sh/install | bash
+if type -a bun >&2;
+then
+    echo "Bun installed"
+else
+    curl -fsSL https://bun.sh/install | bash
+
+    source /home/electron/.bashrc
+fi
