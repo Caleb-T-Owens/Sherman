e2ffb0e9336067f93ebe624dd12592fa669e42cd
Caleb Owens <hello@calebowens.com>
2024-10-28 12:03:51 +0000

    Wezterm config: Remove ligatures and fix fullstream

--- configsets/35-wezterm/.wezterm.lua
+++ configsets/35-wezterm/.wezterm.lua
@@ -13,5 +13,10 @@ config.font_size = 22
 
 config.font = wezterm.font('CommitMonoDotted', { weight = 400 })
 
+-- Disable ligatures
+config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
+
+config.native_macos_fullscreen_mode = true
+
 -- and finally, return the configuration to wezterm
 return config
