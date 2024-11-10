37e3d64dc858ac477f2a2698bb4524c2b8013179
Caleb Owens <hello@calebowens.com>
2024-11-10 02:01:00 +0100

    Link to notes in website

--- projects/website3/src/app/main-layout/navigation.component.ts
+++ projects/website3/src/app/main-layout/navigation.component.ts
@@ -12,6 +12,7 @@ import { RouterLink } from '@angular/router'
         <ul>
           <li><a routerLink="/">Home</a></li>
           <li><a routerLink="/phrases">Phrases</a></li>
+          <li><a href="https://notes.cto.je">Notes</a></li>
         </ul>
       </nav>
       <nav>
