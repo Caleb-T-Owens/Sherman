208a5905623128bafc39b88cbb9fb726d73d4d65
Caleb Owens <hello@calebowens.com>
2024-12-01 10:56:51 +0100

    Remove extra transpose

--- projects/advent_of_code/year2024/day01/main.rb
+++ projects/advent_of_code/year2024/day01/main.rb
@@ -1,7 +1,5 @@
-entries = File.readlines("aoc-input.txt", chomp: true).map { _1.split("   ").map(&:to_i) }.transpose.map(&:sort).transpose
+a, b = File.readlines("aoc-input.txt", chomp: true).map { _1.split("   ").map(&:to_i) }.transpose.map(&:sort)
 
-puts entries.sum { |(a, b)| (a - b).abs }
-
-a, b = entries.transpose
+puts [a, b].transpose.sum { |(a, b)| (a - b).abs }
 
 puts a.sum { |x| b.count { _1 == x } * x }
