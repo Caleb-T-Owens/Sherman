3a1110b90feb65f77edcac93309261a913ba8f9b
Caleb Owens <hello@calebowens.com>
2024-12-02 07:34:34 +0100

    tidy up to_is

--- projects/advent_of_code/year2024/day02/main.rb
+++ projects/advent_of_code/year2024/day02/main.rb
@@ -3,24 +3,24 @@ test_input =  File.readlines("aoc-test.txt", chomp: true)
 
 def one(input)
   input.count { 
-    as = _1.split(" ")
-    as.each_cons(2).all? { |(a, b)| b.to_i > a.to_i &&  b.to_i - a.to_i >= 1 && b.to_i - a.to_i <= 3 } ||
-    as.each_cons(2).all? { |(a, b)| a.to_i > b.to_i && a.to_i - b.to_i >= 1 && a.to_i - b.to_i <= 3 }
+    as = _1.split(" ").map(&:to_i)
+    as.each_cons(2).all? { |(a, b)| b > a &&  b - a >= 1 && b - a <= 3 } ||
+    as.each_cons(2).all? { |(a, b)| a > b && a - b >= 1 && a - b <= 3 }
   }
 end
 
 def two(input)
   input.count { 
-    as = _1.split(" ")
+    as = _1.split(" ").map(&:to_i)
     list_with_one_missing(as).any? { |x|
-      x.each_cons(2).all? { |(a, b)| b.to_i > a.to_i && b.to_i - a.to_i >= 1 && b.to_i - a.to_i <= 3 } ||
-      x.each_cons(2).all? { |(a, b)| a.to_i > b.to_i && a.to_i - b.to_i >= 1 && a.to_i - b.to_i <= 3 }
+      x.each_cons(2).all? { |(a, b)| b > a && b - a >= 1 && b - a <= 3 } ||
+      x.each_cons(2).all? { |(a, b)| a > b && a - b >= 1 && a - b <= 3 }
     } 
   }
 end
 
 def list_with_one_missing(list)
-  list.length.times.map { (_1 != 0 ? list[0..(_1 - 1)] : []) + ((_1 != list.length - 1) ? list[(_1 + 1)..list.length] : [])} + [list]
+  list.length.times.map { (_1 != 0 ? list[0..(_1 - 1)] : []) + ((_1 != list.length - 1) ? list[(_1 + 1)..list.length] : [])}
 end
 
 puts "test:"
