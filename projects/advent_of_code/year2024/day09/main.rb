9e422736ca375d668d0aec440a79b08dd4aeecb0
Caleb Owens <hello@calebowens.com>
2024-12-08 20:58:21 +0100

    Added day9 template

---
+++ projects/advent_of_code/year2024/day09/main.rb
@@ -0,0 +1,17 @@
+input = File.readlines("aoc-input.txt", chomp: true)
+test_input =  File.readlines("aoc-test.txt", chomp: true)
+
+def one(input)
+  input.size
+end
+
+def two(input)
+  input.size
+end
+
+puts "test:"
+puts one(test_input)
+puts two(test_input)
+puts "input:"
+puts one(input)
+puts two(input)
