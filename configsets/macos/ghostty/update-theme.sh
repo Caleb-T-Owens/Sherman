5997e133c9a48b159384306315d0f5b42011c518
Caleb Owens <caleb@gitbutler.com>
2025-10-08 20:19:44 +0200

    Add update theme script
    This should be a global `sherman update-theme` command

---
+++ configsets/macos/ghostty/update-theme.sh
@@ -0,0 +1,10 @@
+# We want to clean out the alacritty folder
+echo "Removing old configs"
+if [ -e $HOME/.config/ghostty ]
+then
+    rm -r $HOME/.config/ghostty
+fi
+
+echo "Installing new configs"
+cp -R ghostty $HOME/.config/ghostty
+
