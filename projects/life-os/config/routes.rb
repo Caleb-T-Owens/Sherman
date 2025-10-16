00d4d2497bb10e3ccdd23db6615cdf44f80d86c0
Caleb Owens <caleb@gitbutler.com>
2025-10-16 15:49:31 +0200

    Stuuuufff

--- projects/life-os/config/routes.rb
+++ projects/life-os/config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   resource :session
   resources :passwords, param: :token
+  resources :users, only: [ :new, :create ]
+
+  root "pages#home"
+  get "dashboard", to: "pages#dashboard"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -10,7 +15,4 @@ Rails.application.routes.draw do
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
