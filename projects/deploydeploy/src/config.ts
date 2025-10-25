4d621f254e20c056d9364e7fed955158f66f550f
Caleb Owens <caleb@gitbutler.com>
2025-10-25 13:45:37 +0100

    Deploydeploy

---
+++ projects/deploydeploy/src/config.ts
@@ -0,0 +1,28 @@
+import { $ } from "bun";
+
+export type Config = {
+    artifacts: Record<string, string>
+};
+
+export type Project = {
+    name: string,
+    config?: Config
+}
+
+export async function readConfigs(servicesPath: string): Promise<Project[]> {
+    const serviceFolders = (await $`find ${servicesPath} -maxdepth 1 -mindepth 1 -type d`.text()).trim().split("\n")
+    const out = [];
+    for await (const s of serviceFolders) {
+        let config: Config | undefined;
+        if ((await $`test -f ${s}/config.json`.quiet()).exitCode === 0) {
+            config = JSON.parse(await $`cat ${s}/config.json`.text())
+        }
+
+        out.push({
+            name: s.split("/").at(-1)!,
+            config
+        })
+    }
+
+    return out;
+}
---
+++ projects/deploydeploy/src/docker.ts
@@ -0,0 +1,12 @@
+import { $ } from "bun"
+
+export type DockerLsEntry = {
+    Name: string,
+    Status: string,
+    ConfigFiles: string
+}
+
+export async function dockerLs(): Promise<DockerLsEntry[]> {
+    const output = JSON.parse((await $`docker compose ls --format=json`.text()))
+    return output
+}
