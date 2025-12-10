c099acc16ace5d98d155f5c4cc41161356888b2a
Caleb Owens <caleb@gitbutler.com>
2025-12-10 11:28:55 +0100

    Start of some project info

--- projects/not-a-forge/app/models/repository.rb
+++ projects/not-a-forge/app/models/repository.rb
@@ -4,10 +4,16 @@ class Repository < ApplicationRecord
 
   validates :owner, presence: true
   validates :name, presence: true
-  validates :owner, uniqueness: { scope: [:user_id, :name], message: "and name combination already exists" }
+  validates :owner, uniqueness: { scope: [ :user_id, :name ], message: "and name combination already exists" }
+
+  delegate :api, to: :token
 
   # Returns the full repository name in owner/name format
   def full_name
     "#{owner}/#{name}"
+  end
+
+  def info
+    @info ||= api.repository(full_name)
   end
 end
