9ec9b17a8ca354eadfffc4a47827dd70f7e4aba6
Caleb Owens <caleb@gitbutler.com>
2025-12-23 21:11:08 +0100

    Recipies

--- projects/ctech/src/main/java/je/cto/ctech/block/BasicMachineBlock.java
+++ projects/ctech/src/main/java/je/cto/ctech/block/BasicMachineBlock.java
@@ -1,6 +1,10 @@
 package je.cto.ctech.block;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.modificationstation.stationapi.api.recipe.CraftingRegistry;
 import net.modificationstation.stationapi.api.template.block.TemplateBlock;
 import net.modificationstation.stationapi.api.util.Identifier;
 
@@ -12,5 +16,19 @@ public class BasicMachineBlock extends TemplateBlock {
     @Override
     public boolean isFullCube() {
         return false;
+    }
+
+    /**
+     * Registers the crafting recipe: 8 iron in a circle = 1 block.
+     * III
+     * I I
+     * III
+     */
+    public static void registerRecipe(Block result) {
+        CraftingRegistry.addShapedRecipe(
+            new ItemStack(result, 1),
+            "III", "I I", "III",
+            'I', new ItemStack(Item.IRON_INGOT)
+        );
     }
 }
