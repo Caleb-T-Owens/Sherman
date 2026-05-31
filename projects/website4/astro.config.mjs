539019fcf1cb1997d2089c1f64d4549862173966
Caleb Owens <caleb@gitbutler.com>
2026-05-31 19:06:43 +0100

    Add Ketex

--- projects/website4/astro.config.mjs
+++ projects/website4/astro.config.mjs
@@ -2,6 +2,8 @@
 import { defineConfig } from "astro/config";
 
 import mdx from "@astrojs/mdx";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 
 // https://astro.build/config
 export default defineConfig({
@@ -14,5 +16,7 @@ export default defineConfig({
         light: "github-light",
       },
     },
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
   },
 });
